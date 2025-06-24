60a88295fd4dac5119aa38327a9b3ef444ed6f44
sandhyareddy451 <vemula.sandhya51@gmail.com>
2025-06-24 11:42:31 +0530

    folder structre edit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    
+    
+    name = "Generative AI project",
+    version = "0.0.0",
+    author = "vemula sandhya",
+    author_email = "vemula,sandhya51@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
